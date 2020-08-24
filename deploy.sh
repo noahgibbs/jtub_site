ef4686db1cc0f2141d90101d374fefae75322e0e
Noah Gibbs <the.codefolio.guy@gmail.com>
2020-08-24 14:20:22 +0100

    Fix deploy shellscript

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ set -e
 set -x
 
 git push
-ssh deployer@madrubyscience.com "PATH=/home/deployer/.rbenv/bin:/home/deployer/.rbenv/shims:$PATH; cd checkouts/jtub_site && git pull && bundle && cd assets && ./process.rb && cd .. && bundle exec middleman build"
+ssh deployer@madrubyscience.com "PATH=/home/deployer/.rbenv/bin:/home/deployer/.rbenv/shims:$PATH; cd checkouts/jtub_site && git pull && bundle && bundle exec middleman build"
